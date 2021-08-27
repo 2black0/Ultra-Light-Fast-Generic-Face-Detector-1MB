919c637ca51a13d7b2a55753420744cdb3b6a636
Ardy Seto <ardyseto@uny.ac.id>
2021-08-27 07:01:40 +0700

    Update video-face.py

--- video-face.py
+++ video-face.py
@@ -36,29 +36,6 @@ cap = cv2.VideoCapture(2)
 cap.set(3, 640)
 cap.set(4, 480)
 
-'''(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
-if int(major_ver)  < 3 :
-    fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
-    print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
-else :
-    fps = video.get(cv2.CAP_PROP_FPS)
-    print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
-
-num_frames = 120
-print("Capturing {0} frames".format(num_frames))
-
-start = time.time()
-for i in range(0, num_frames) :
-    ret, frame = video.read()
-end = time.time()
-
-seconds = end - start
-print ("Time taken : {0} seconds".format(seconds))
-
-fps  = num_frames / seconds
-print("Estimated frames per second : {0}".format(fps))'''
-
-
 class_names = [name.strip() for name in open(label_path).readlines()]
 num_classes = len(class_names)
 test_device = args.test_device
@@ -84,7 +61,6 @@ net.load(model_path)
 timer = Timer()
 sum = 0
 
-##
 prev_frame_time = 0
 new_frame_time = 0
 
